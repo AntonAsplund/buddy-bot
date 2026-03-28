beec65f7debabde13efb8d944c5afd99bc05005a
Anton Asplund <57093970+AntonAsplund@users.noreply.github.com>
2026-03-28 02:00:09 +0100

    Makes use of message handler and adds available tools for anthropic client

--- src/core/claude.js
+++ src/core/claude.js
@@ -1,7 +1,27 @@
 require('dotenv').config();
 const Anthropic = require('@anthropic-ai/sdk');
 const client = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY });
-const conversations = {};
+const conversations = {}; // TODO: Update to make use of DB for context persistance between messages.
+const {
+    JSON_DESCRIPTION_ADD,
+    JSON_DESCRIPTION_LIST,
+    JSON_DESCRIPTION_MARK_DONE,
+    JSON_DESCRIPTION_CLEAR_BOUGHT,
+    handleShoppingAdd,
+    handleShoppingShowList, 
+    handleShoppingDone, 
+    handleShoppingClear
+
+ } = require('../handlers/message-handler');
+
+// const determineAvailableToolsForUserGroup = (userGroup) => {
+//     // For example, you could have different tools available for 'admin' vs 'user'
+//     if (userGroup === 'admin') {
+//         return ['shopping', 'activity_planner', 'general_chat'];
+//     }
+    
+//     return ['shopping'];
+// }
 
 async function chat(userId, userMessage, systemPrompt) {
     if (!conversations[userId]) {
@@ -15,18 +35,47 @@ async function chat(userId, userMessage, systemPrompt) {
         conversations[userId] = conversations[userId].slice(-20);
     }
 
-    // TODO: Add retry pattern
     const response = await client.messages.create({
         model: 'claude-haiku-4-5',
         max_tokens: 1024,
         system: systemPrompt,
         messages: conversations[userId],
-    });
+        tools: [
+            JSON_DESCRIPTION_ADD,
+            JSON_DESCRIPTION_LIST,
+            JSON_DESCRIPTION_MARK_DONE,
+            JSON_DESCRIPTION_CLEAR_BOUGHT
+        ]
+    },
+    { maxRetries: 5 });
+
+    if (response.content[0].type === 'tool_use') {
+        const toolToUse = response.content[0];
+        const toolResult = await executeTool(toolToUse.name, toolToUse.input, userId);
+
+        console.log(`Tool ${toolToUse.name} executed with result: ${toolResult}`);
+        console.log('Full tool response:', toolResult);
+    };
 
     const reply = response.content[0].text;
     conversations[userId].push({ role: 'assistant', content: reply });
 
     return reply;
+}
+
+const executeTool = (toolName, toolInput, userId) => {
+    switch (toolName) {
+        case 'add_to_shopping_list':
+            return handleShoppingAdd(toolInput.item, toolInput.quantity, userId);
+        case 'list_shopping_items':
+            return handleShoppingShowList();
+        case 'mark_item_as_bought':
+            return handleShoppingDone([toolInput.item_number], userId);
+        case 'clear_bought_items':
+            return handleShoppingClear();
+        default:
+            return `Unknown tool: ${toolName}`;
+    }
 }
 
 function clearHistory(userId) {
